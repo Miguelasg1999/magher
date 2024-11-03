87b01d115dda66f0d32090c878cc7d8b735e1001
Miguelasg1999 <miguelasg1999@gmail.com>
2024-11-03 15:20:49 -0300

    edit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
+
+  async login(email:string, password:string){
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  async register(email:string, password:string){
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+  }
+
+  async logout(){
+    return this.afAuth.signOut();
+  }
+
+  getUser(){}
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCP6W9greYhGc_I_AkyBgEhgcZnyA1LnW0",
+    authDomain: "magher-2936c.firebaseapp.com",
+    projectId: "magher-2936c",
+    storageBucket: "magher-2936c.firebasestorage.app",
+    messagingSenderId: "970679484162",
+    appId: "1:970679484162:web:2f5864ccd3d9a1285e0c1b"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
