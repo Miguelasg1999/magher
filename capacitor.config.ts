307b17f7609d66582cc4b4aea90352bee0d00c7e
Miguelasg1999 <miguelasg1999@gmail.com>
2024-11-03 14:47:16 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'magher',
+  webDir: 'www'
+};
+
+export default config;
