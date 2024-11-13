c6345a63e87d246fdb96a63db7260bdf63a1a55f
privateNelli <kilito.hernandez@gmail.com>
2024-11-13 10:12:03 -0300

    legibilidad y botones arreglados

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { Loader } from '@googlemaps/js-api-loader';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 //import Swiper from 'swiper';
 
 @Component({
@@ -14,6 +16,13 @@ export class InicioPage implements OnInit {
   swiperRef: ElementRef | undefined;
   swiper?: Swiper;*/
 
+  
+
+
+    // Obtenemos el estado de autenticación del usuario
+    
+
+
   map: google.maps.Map | undefined;  // Definimos la variable del mapa
   lat: number = -38.76368335586436;  // Latitud de ejemplo (Nueva York)
   lng: number = -72.60441911789803; // Longitud de ejemplo (Nueva York)
@@ -23,8 +32,10 @@ export class InicioPage implements OnInit {
     'https://images.crowdspring.com/blog/wp-content/uploads/2023/05/16174534/bakery-hero.png',
     'https://media.istockphoto.com/id/518468635/photo/french-pastries-on-display-a-confectionery-shop.jpg?s=612x612&w=0&k=20&c=av7rYRCLckvDp6s9sGmA_S9Lp_fTSDSSOtmR0wWJ8nY=',
     'https://cdn.vox-cdn.com/thumbor/asoX4fSv_-t7PapIPVK1dsrPM7Y=/0x0:3024x4032/1200x900/filters:focal(1691x1908:2173x2390)/cdn.vox-cdn.com/uploads/chorus_image/image/63894484/IMG_9124.63.jpeg']
-
-  constructor(private aService: AuthService, private router: Router) { }
+  
+    user$: Observable<any>;
+  constructor(private aService: AuthService, private router: Router, private afAuth: AngularFireAuth) { 
+    this.user$ = this.afAuth.authState;}
 
   logout(){
     this.aService.logout();
