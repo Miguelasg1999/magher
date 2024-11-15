b0c6fdaafb7f34c399c80106fa3f9a88bd7c3071
privateNelli <kilito.hernandez@gmail.com>
2024-11-15 13:51:50 -0300

    android sdk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebaseConfig : {
+    apiKey: "AIzaSyCP6W9greYhGc_I_AkyBgEhgcZnyA1LnW0",
+    authDomain: "magher-2936c.firebaseapp.com",
+    projectId: "magher-2936c",
+    storageBucket: "magher-2936c.firebasestorage.app",
+    messagingSenderId: "970679484162",
+    appId: "1:970679484162:web:2f5864ccd3d9a1285e0c1b"
+  }
 };
